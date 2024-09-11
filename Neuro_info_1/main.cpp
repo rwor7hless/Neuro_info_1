452fb7c9a5852236558215b0db81e8d43f5ace92
rwor7hless <rwor7hless@gmail.com>
2024-09-11 23:32:49 +0300

    add corrupted data 3

--- Neuro_info_1/main.cpp
+++ Neuro_info_1/main.cpp
@@ -10,13 +10,13 @@ void getAcc(std::vector<std::vector<int>>& data, neuron& n) {
 
 int main() {
    
-    auto [training_data, targets] = readTrainDataFromFile("data/train_data_corrupted_2.txt");
+    auto [training_data, targets] = readTrainDataFromFile("data/train_data_corrupted_3.txt");
     shuffle_data(training_data, targets);
 
 
     neuron neuro(9, -1, 1, 25);  // Создаем нейрон с 9 весами в диапазоне от -1 до 1, порогом классификации 25 
     std::cout << "Веса до обучения:   "; neuro.printW();
-    neuro.fit(training_data, targets, 1, 1);
+    neuro.fit(training_data, targets, 5, 1);
     std::cout << "Веса после обучения:"; neuro.printW();
     getAcc(training_data, neuro);
 
