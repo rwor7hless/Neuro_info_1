c19eb970be5c37034e64aa5fa0b66b8d092ca476
Roman <rwor7hless@gmail.com>
2024-09-13 12:04:55 +0300

    eng traslate

--- Neuro_info_1/main.cpp
+++ Neuro_info_1/main.cpp
@@ -3,21 +3,20 @@
 
 void getAcc(std::vector<std::vector<int>>& data, neuron& n) {
     std::cout  
-              << "\nКоличество ошибок:\t |" << n.getErrors()
-              << "\nДанных для обучения:\t |" << data.size()
-              << "\nТочность обучния:\t |"  << ((data.size() - n.getErrors())*1.0f / data.size()) * 100 << " %\n";
+              << "\nErrors count:\t\t |" << n.getErrors()
+              << "\nTrain data size:\t |" << data.size()
+              << "\nfit accuracy:\t\t |"  << ((data.size() - n.getErrors())*1.0f / data.size()) * 100 << " %\n";
 }
 
 int main() {
-   
     auto [training_data, targets] = readTrainDataFromFile("data/train_data_corrupted_5.txt");
     shuffle_data(training_data, targets);
 
 
     neuron neuro(9, -1, 1, 25);  // Создаем нейрон с 9 весами в диапазоне от -1 до 1, порогом классификации 25 
-    std::cout << "Веса до обучения:   "; neuro.printW();
+    std::cout << "Weights before fit:\t"; neuro.printW();
     neuro.fit(training_data, targets, 5, 1);
-    std::cout << "Веса после обучения:"; neuro.printW();
+    std::cout << "Weights after fit:\t"; neuro.printW();
     getAcc(training_data, neuro);
 
     std::vector<std::vector<int>> test_data = readTestDataFromFile("data/test_data.txt");
